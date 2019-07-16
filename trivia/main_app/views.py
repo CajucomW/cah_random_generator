bd7fa3718027711408cca1f1cd67df11e69a60c2
Wence Cajucom <cajucom.w@gmail.com>
2019-07-16 12:49:00 -0700

    Quiz currently working with single questions. Attemtps at adding more questions inadvertently appends old answers with new answers.

--- trivia/main_app/views.py
+++ trivia/main_app/views.py
@@ -4,15 +4,19 @@ from django.shortcuts import render
 
 response = requests.get('https://opentdb.com/api.php?amount=1')
 data = response.json()
+user_answer = None
+answers = []
+
+print("Welcome to Our Group Project!")
+username = input("Please enter a Username: ")
 
 def quiz():
-    for item in data['results']:
-        correct = item['correct_answer']
-        choices = item['incorrect_answers']
-        print(item['question'])
-        
-        answers = []
 
+    for item in data['results']:            # get to the right list
+        print(item['question'])
+        correct = item['correct_answer']    # reference keys for correct answers
+        choices = item['incorrect_answers'] # reverence keys for incorrect answers
+        
         for choice in choices:
             answers.append(choice)      # this adds the wrong answers to the list "answers"
         answers.append(correct)         # this adds the right answer to he list "answers"
@@ -21,14 +25,8 @@ def quiz():
         print(answers)
         
         user_answer = input("Type your answer here: ")
-        if user_answer in item['correct_answer']:
+        if user_answer in correct:
             print("You got it!")
         else:
-            print("Sorry. The answer is", correct, ".")
+            print("Sorry. The answer is", correct + ".")
 quiz()
-
-# i = 0
-# while i < 3:
-#     quiz()
-#     i += 1
-# Create your views here.
