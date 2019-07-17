8575c1becefae5ef880ef48b6b4aed7db239cb85
Wence Cajucom <cajucom.w@gmail.com>
2019-07-16 19:08:32 -0700

    Fixed problem with appending to answers list.

--- trivia/main_app/models.py
+++ trivia/main_app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class quiz(models.Model):
+    username = models.CharField(max_length=100)
--- trivia/main_app/views.py
+++ trivia/main_app/views.py
@@ -2,21 +2,26 @@ import requests
 import random
 from django.shortcuts import render
 
-response = requests.get('https://opentdb.com/api.php?amount=1')
+response = requests.get('https://opentdb.com/api.php?amount=3')
 data = response.json()
 user_answer = None
-answers = []
 
-print("Welcome to Our Group Project!")
-username = input("Please enter a Username: ")
+
+def welcome():
+
+    print("Welcome to Our Group Project!")
+    username = input("Please enter a Username: ")
+    
+welcome()
 
 def quiz():
-
+    # while True:
     for item in data['results']:            # get to the right list
         print(item['question'])
         correct = item['correct_answer']    # reference keys for correct answers
         choices = item['incorrect_answers'] # reverence keys for incorrect answers
         
+        answers = []
         for choice in choices:
             answers.append(choice)      # this adds the wrong answers to the list "answers"
         answers.append(correct)         # this adds the right answer to he list "answers"
@@ -29,4 +34,5 @@ def quiz():
             print("You got it!")
         else:
             print("Sorry. The answer is", correct + ".")
+        
 quiz()
